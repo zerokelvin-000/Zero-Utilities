7689e347bbed1badea09feee103d0718e9739f66
ZK <zerokelvin.personal@gmail.com>
2025-01-19 23:38:44 +0100

    aggiunte primi metodi per i vettori

--- PHP/vectors.php
+++ PHP/vectors.php
@@ -1,6 +1,38 @@
 <?php
     namespace ZKutils\Vectors;
 
-    trait Vectors{
+    require "matrices.php";
 
+    use ZKutils\Matrix\Matrix as matrix;
+
+    trait Vectors_2D{
+        public static function scale_vector($vector, $scalar){
+            foreach($vector as $index => $object){
+                $vector[$index] = $object * $scalar;
+            }
+
+            return $vector;
+        }
+
+        public static function add_vectors($vector1, $vector2){
+            $result = matrix::add([$vector1, $vector2]);
+
+            return $result;
+        }
+
+        public static function subtract_vectors($vector1, $vector2){
+            $result = matrix::subtract([$vector1, $vector2]);
+
+            return $result;
+        }
+
+        public static function multiply_vectors($vector1, $vector2){
+            $result = matrix::multiply($vector1, $vector2);
+
+            return $result;
+        }
+    }
+
+    trait Vectors{
+        use Vectors_2D;
     }
