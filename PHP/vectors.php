cc59a84533c927bf3cf4e8ff23160c6f74dd17c4
ZK <zerokelvin.personal@gmail.com>
2025-01-22 12:52:57 +0100

    vettori

--- PHP/vectors.php
+++ PHP/vectors.php
@@ -2,33 +2,49 @@
     namespace ZKutils\Vectors;
 
     require "matrices.php";
-    require "points.php";
 
-    use ZKutils\Points\Point_2D;
     use ZKutils\Matrix\Matrix;
 
-    trait Vectors_2D{
-        public static function scale_vector(Point_2D $original_point, $scalar){
-            return new Point_2D($original_point->posx * $scalar, $original_point->posy * $scalar);
-        }
+    trait Point{
+        public float $x;
+        public float $y;
+        public float $z;
 
-        public static function add_vectors(Point_2D $point1, Point_2D $point2){
-            return new Point_2D($point1->posx + $point2->posx, $point1->posy + $point2->posy); 
-
-            return $result;
-        }
-
-        public static function subtract_vectors(Point_2D $point1, Point_2D $point2){
-            return new Point_2D($point1->posx - $point2->posx, $point1->posy - $point2->posy); 
-
-            return $result;
-        }
-
-        public static function vector_magnitude(Point_2D $point1, Point_2D $point2){
-            self::subtract_vectors($point1, $point2);
+        public function set_2D_point($x, $y){
+            $this->x = $x;
+            $this->y = $y;
         }
     }
 
-    class Vectors{
-        use Vectors_2D;
+    trait Vector_2D{
+        public static function create_vector($x, $y){
+            $point = new Point();
+            $point->set_2D_point($x, $y);
+        }
+
+        public static function scale_vector(Point $original_point, $scalar){
+            return new Point($original_point->x * $scalar, $original_point->y * $scalar);
+        }
+
+        public static function add_vectors(Point $point1, Point $point2){
+            return new Point($point1->posx + $point2->posx, $point1->posy + $point2->posy);
+        }
+
+        public static function subtract_vectors(Point $point1, Point $point2){
+            return new Point($point1->posx - $point2->posx, $point1->posy - $point2->posy);
+        }
+
+        public static function vector_dot_product(Point $point1, Point $point2){
+            return new Point();
+        }
+    }
+
+    class Vector{
+        use Vector_2D;
+
+        public Point $position;
+
+        public function set_position($x, $y){
+            $this->position = new Point($x, $y);
+        }
     }
